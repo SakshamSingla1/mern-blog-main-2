c993c9631a4bbd4aa343d673bef9cd748e186698
SakshamSingla1 <sksingla135@gmail.com>
2024-03-06 20:08:55 +0530

    Initial commit

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,23 +1,24 @@
 import { Button } from 'flowbite-react';
+import img  from '../images/portfolio.png';
 
 export default function CallToAction() {
   return (
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Welcome to my portfolio!
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Explore my work and projects in JavaScript.
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://sakshamsingla.me" target='_blank' rel='noopener noreferrer'>
+                    Check it out!
                 </a>
             </Button>
         </div>
         <div className="p-7 flex-1">
-            <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+            <img src={img} alt="JavaScript" />
         </div>
     </div>
   )
